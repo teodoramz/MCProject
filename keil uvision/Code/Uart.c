69418d65b393cd568370b97da93a1d26617b3da4
teodoramz <amzteodor@gmail.com>
2022-12-22 22:06:29 +0200

    ready for documentation stage

--- keil uvision/Code/Uart.c
+++ keil uvision/Code/Uart.c
@@ -1,12 +1,5 @@
 #include "Uart.h"
 
-char c; 
-char buffer[32];
-
-int write, read;
-int print_buffer;
-int full;
-
 void UART0_Transmit(uint8_t data)
 {
 	//Punem in asteptare pana cand registrul de transmisie a datelor nu este gol
@@ -15,16 +8,11 @@ void UART0_Transmit(uint8_t data)
 	
 }
 
-uint8_t UART0_receive(void)
-{
-	//Punem in asteptare pana cand registrul de receptie nu este plin
-	while(!(UART0->S1 & UART0_S1_RDRF_MASK)){}
-				return UART0->D;
-	
-}
-
 void UART0_Init(uint32_t baud_rate)
 {
+	uint32_t osr;
+	uint16_t sbr;
+	uint8_t temp;
 	
 	//Setarea sursei de ceas pentru modulul UART
 	SIM->SOPT2 |= SIM_SOPT2_UART0SRC(01);
@@ -38,8 +26,7 @@ void UART0_Init(uint32_t baud_rate)
 	
 	//Fiecare pin pune la dispozitie mai multe functionalitati 
 	//la care avem acces prin intermediul multiplexarii
-	PORTA->PCR[1] = ~PORT_PCR_MUX_MASK;
-	PORTA->PCR[1] = PORT_PCR_ISF_MASK | PORT_PCR_MUX(2); // Configurare RX pentru UART0
+
 	PORTA->PCR[2] = ~PORT_PCR_MUX_MASK;
 	PORTA->PCR[2] = PORT_PCR_ISF_MASK | PORT_PCR_MUX(2); // Configurare TX pentru UART0
 	
@@ -48,7 +35,7 @@ void UART0_Init(uint32_t baud_rate)
 	UART0->C2 &= ~((UART0_C2_RE_MASK) | (UART0_C2_TE_MASK)); 
 	
 	//Configurare Baud Rate
-	uint32_t osr = 15; // Over-Sampling Rate (numarul de esantioane luate per bit-time)
+  osr = 15; // Over-Sampling Rate (numarul de esantioane luate per bit-time)
 	
 	//SBR - vom retine valoarea baud rate-ului calculat pe baza frecventei ceasului de sistem
 	// 	 SBR  -		b16 b15 b14 [b13 b12 b11 b10 b09		b08 b07 b06 b05 b04 b03 b02 b01] &
@@ -56,11 +43,10 @@ void UART0_Init(uint32_t baud_rate)
 	//            0   0   0    b13 b12 b11 b10 b09    0   0   0   0   0   0   0   0 >> 8
 	//   BDH  -   0   0   0    b13 b12 b11 b10 b09
 	//   BDL  -   b08 b07 b06  b05 b04 b03 b02 b01
-	uint16_t sbr = (uint16_t)((DEFAULT_SYSTEM_CLOCK)/(baud_rate * (osr+1)));
-	uint8_t temp = UART0->BDH & ~(UART0_BDH_SBR(0x1F));
+  sbr = (uint16_t)((DEFAULT_SYSTEM_CLOCK)/(baud_rate * (osr+1)));
+	temp = UART0->BDH & ~(UART0_BDH_SBR(0x1F));
 	UART0->BDH = temp | UART0_BDH_SBR(((sbr & 0x1F00)>> 8));
 	UART0->BDL = (uint8_t)(sbr & UART_BDL_SBR_MASK);
-	UART0->C4 |= UART0_C4_OSR(osr);
 	
 	
 	//Setare numarul de biti de date la 8 si fara bit de paritate
@@ -70,14 +56,7 @@ void UART0_Init(uint32_t baud_rate)
 	UART0->C2 |= UART0_C2_TIE(0);
 	UART0->C2 |= UART0_C2_TCIE(0);
 	
-	//Activare intreruperi la receptie
-	UART0->C2 |= UART0_C2_RIE(1);
-	
-	UART0->C2 |= ((UART_C2_RE_MASK) | (UART_C2_TE_MASK));
-	
-	NVIC_EnableIRQ(UART0_IRQn);
+	UART0->C2 |= UART_C2_TE_MASK;
 	
 }
-void UART0_IRQHandler(void) {
 
-}
